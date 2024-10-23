f661583d0816bc3c8af3313a490378f8c805fb33
Xavi <149520332+llotjer@users.noreply.github.com>
2024-10-23 13:52:47 +0200

    Update routes.php
    Added all routes

--- config/routes.php
+++ config/routes.php
@@ -10,5 +10,19 @@
  * '/calendar' => 'calendar#index'
  */
 $routes = array(
-	'/test' => 'test#index'
+	'/test' => 'test#index',
+	'/' => 'task#index',
+	'/lang' => 'task#lang',
+	'/create' => 'task#create',
+	'/read' => 'task#read',
+	'/update' => 'task#update',
+	'/delete' => 'task#delete',
+	'/add' => 'task#add',
+	'/confirmUpdate' => 'task#confirmUpdate',
+	'/updateTask' => 'task#taskUpdate',
+	'/deleteTask' => 'task#taskDelete',
+	'/confirmDelete' => 'task#confirmDelete',
+	'/success' => 'task#success',
+	'/successUpdate' => 'task#successUpdate',
+	'/successDelete' => 'task#successDelete',
 );
